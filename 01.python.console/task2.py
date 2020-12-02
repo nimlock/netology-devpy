ba405cd2ccbe8690b18e2bbcdd7e6f66e8688895
Жиляев Иван Викторович <zhilyaev.ivan@itmh.ru>
2020-12-02 23:14:24 +0500

    ДЗ1-задача2 выполнена

---
+++ 01.python.console/task2.py
@@ -0,0 +1,8 @@
+sqr_lenghth = int(input('Введите длину стороны квадрата: '))
+print('Периметр:', sqr_lenghth*4)
+print('Площадь:', sqr_lenghth*sqr_lenghth)
+
+lenghth = int(input('Введите длину прямоугольника: '))
+width = int(input('Введите ширину прямоугольника: '))
+print('Периметр:', lenghth*2+width*2)
+print('Площадь:', lenghth*width)
