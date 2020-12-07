e4db05f0d3b9ce13f8c275b97bef5e3f0902308b
Жиляев Иван Викторович <zhilyaev.ivan@itmh.ru>
2020-12-07 21:42:12 +0500

    ДЗ3-задача1 дополнена

--- 03.introduce_datatypes/task1.py
+++ 03.introduce_datatypes/task1.py
@@ -2,7 +2,7 @@ boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
 girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
 
 if len(boys) != len(girls):
-    print('Предупреждение, что кто-то может остаться без пары!')
+    print('Предупреждение: кто-то может остаться без пары!')
 elif len(boys) > 0:
     boys = sorted(boys)
     girls = sorted(girls)
@@ -10,3 +10,5 @@ elif len(boys) > 0:
     print('Идеальные пары:')
     for pair in pairs:
         print(pair[0], 'и', pair[1])
+else:
+    print('Ошибка: предоставлены пустые списки!')
